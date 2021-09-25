c0758450dfab2eb5ecaedcac2c6a5f0e40fc1dbb
FranciscoOssian <ossian.neto@gmail.com>
2021-09-24 23:17:15 -0300

    hot bug fix nanme index

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set( 'views', path.join( __dirname, 'public' ) )
 app.engine( 'html', require('ejs').renderFile )
 app.set( 'view engine', 'html' )
 app.use('/', (req, res) => {
-  res.render('index.html')
+  res.render('empty.html')
 })
 //-------------------------------------------------------------
 
